68498ea0c973994540f93602d383073655fb647e
aarav <fallingblackhole@gmail.com>
2026-09-15 21:01:16 -0400

    feat 4: implemented logic for feature 4, all the variables to store information are ready, but untested, error handling is not prepared, and function does not have return yet

--- src/strategies/TaxDeductionStrategy.ts
+++ src/strategies/TaxDeductionStrategy.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '../models.js';
+import { TaxConfig, Transaction } from '../models.js';
 import { TaxConfigService } from '../services/TaxConfigService.js';
 import { AuditStrategy } from './AuditStrategy.js';
 
@@ -13,12 +13,21 @@ export class TaxDeductionStrategy implements AuditStrategy {
   ): Promise<string> {
     // TODO: Feature 4 - Implement this strategy.
     // 1. Call TaxConfigService.getTaxConfig() asynchronously.
+    let config: TaxConfig = await TaxConfigService.getTaxConfig();
     // 2. Filter expenses (amount < 0) that belong to eligible tax-deductible categories.
+    let eligible_arr: Transaction[] = transactions.filter((action) => action.amount < 0 && config.deductibleCategories.includes(action.category));
+    let in_eligible_arr: Transaction[] = transactions.filter((action) => action.amount >= 0 || !config.deductibleCategories.includes(action.category));
+ 
     // 3. Sum total deductible expenses.
-    // 4. Estimate tax savings based on the standard tax rate: total deductible * taxRate.
-    // 5. Estimate sales tax/VAT paid on NON-deductible expenses using standard tax rate.
-    // 6. Format and return a text-based audit report detailing total deductions, savings, VAT estimates, and eligible transactions.
+    let total_deductible = eligible_arr.reduce((sum, action) => sum + Math.abs(action.amount), 0);
+    let taxed_transactions = in_eligible_arr.reduce((sum, action) => sum + Math.abs(action.amount), 0);
 
-    throw new Error('Method not implemented.');
+    // 4. Estimate tax savings based on the standard tax rate: total deductible * taxRate.
+    let savings = total_deductible * config.standardTaxRate;
+    // 5. Estimate sales tax/VAT paid on NON-deductible expenses using standard tax rate.
+    let tax = taxed_transactions * config.standardTaxRate;
+    // 6. Format and return a text-based audit report detailing total deductions, savings, VAT estimates, and eligible transactions.
+    let report = "";
+
   }
 }
